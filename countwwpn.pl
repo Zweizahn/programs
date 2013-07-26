7ca3d0371fedba8a902bfa8a1dcbd0cad966eab0
Erik Blosze <erik.blosze@falconstor.com>
2013-07-26 17:08:36 +0200

    Included check of start/stop date & time

--- countwwpn.pl
+++ countwwpn.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Last change 	24.Jul 2013
+# Last change 	26.Jul 2013
 # Author	Erik Blosze
 #
 use strict;
@@ -35,12 +35,12 @@ sub INSERT 	# Inserts the data and increases the count
 	foreach my $KEY (@KEYS) {
 		if ($WWPN eq $KEY) {
 			$ARRAY->{$KEY}++;
-			$ADD = "NO";
+			$ADD = "NO";	# Value was found before, just increase count
 			last;
 		}
 	}
 
-	if ($ADD eq "YES") {
+	if ($ADD eq "YES") {		# New values so create new entry
 		$ARRAY->{$WWPN} = 1;
 	}
 	return;
@@ -58,6 +58,19 @@ sub OUTPUT	# Prints the output
 	return;
 }
 
+sub DATECONVERT {		# Changes date string "<Month> <Day> <Hour>:<Minute>:<Second>" to a scalar
+	my ($MONTH, $DAY, $TMP) = @_;
+	my $HOUR     = scalar (substr($TMP,0,2)) * 10000;
+	my $MINUTE   = scalar (substr($TMP,3,2)) * 100;
+	my $SECOND   = scalar (substr($TMP,6,2));
+	my %MON2NUM  = qw(Jan 1  Feb 2  Mar 3  Apr 4  May 5  Jun 6 Jul 7  Aug 8  Sep 9  Oct 10 Nov 11 Dec 12);
+	my $MONTHNUM = $MON2NUM{$MONTH} * 100000000;
+	my $DAYNUM   = scalar ($DAY) * 1000000;
+	my $RETURN   = $MONTHNUM + $DAYNUM + $HOUR + $MINUTE + $SECOND;
+	return $RETURN;
+}
+
+#####################################################################################
 #### Main programm ####
 # Check input parameters
 my $LENGTH=scalar @ARGV;
@@ -72,27 +85,38 @@ if (-d $FILE) {
 }
 
 # Check for additional parameters on the command line
-my %OPTS;
-my $START = '';
-my $END   = '';
-if ($LENGTH > 1) {
-say "Calling getopt";
-	getopt('es',\%OPTS);
-	my $START = $OPTS->{'s'};
-	my $END   = $OPTS->{'e'};
-} 
-say "LENGTH is $LENGTH";
-say "START is \"$START\"";
-say "END is \"$END\"";
+shift @ARGV; 		# Removes filename from the argument list
+my %OPTS  = ();
+my $START;
+my $END;
+my @FIELDS;
+if ($LENGTH > 1) {		# Start/End date set?
+	getopts("e:s:",\%OPTS);
+	@FIELDS = split(/ +/,$OPTS{'e'});
+	$END    = DATECONVERT($FIELDS[0],$FIELDS[1],$FIELDS[2]);
+	@FIELDS = split(/ +/,$OPTS{'s'});
+	$START  = DATECONVERT($FIELDS[0],$FIELDS[1],$FIELDS[2]);
+}  else {
+	$END   = '1300000000';	# Bigger than the biggest possible DATECONVERT value
+	$START = '0';
+}
 
 # *****************
 while (<$FILEHANDLE>) {
 	$NrLines++;
 	if (!m/SCSI command abort/) {next}; 	#Leave while loop if no SCSI command abort can be found
-	chomp;
-	my @FIELDS = split;
-	my $LENGTH = scalar @FIELDS;
-	if ($LENGTH < 14) {next}; 		#Leave while loop if no SCSI command abort can be found
+
+	@FIELDS = split;
+	$LENGTH = scalar @FIELDS;
+	if ($LENGTH < 14) {next}; 		#Leave while loop if not enough fields are found
+
+# Check start/end dates 
+	my $DATE = DATECONVERT($FIELDS[0],$FIELDS[1],$FIELDS[2]);
+	if ($DATE < $START || $END < $DATE) {
+		next;
+	}
+
+# Now checking the fields
 	my $STATUS = $FIELDS[13];
 	my $WWPN   = CONVERT($FIELDS[14]);
 	my $TARGET = CONVERT($FIELDS[17]);
